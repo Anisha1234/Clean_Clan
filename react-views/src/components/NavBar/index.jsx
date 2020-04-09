201eabcd86a8c906b0e1d42c36bd1e4597a727a9
Lan Trung Nguyen <lantrungsir28@gmail.com>
2020-04-09 16:49:39 +0700

    fix nav link to prevent re-render

--- react-views/src/components/NavBar/index.jsx
+++ react-views/src/components/NavBar/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
@@ -12,15 +13,11 @@ const givenPaths = ['/timeline', '/issue', '/ranking'];
 const givenLinkName = ['Timeline', 'Issue', 'Ranking'];
 
 const NavBar = () => {
-  const [activePath, setActivePath] = useState('');
   const userPic = useSelector((state) => state.user.data.image.current);
   const userName = useSelector((state) => state.user.data.name);
-  useEffect(() => {
-    setActivePath(window.location.pathname);
-  }, []);
   return (
     <Navbar expand="md" bg="dark" variant="dark" fixed="top" className="my-nav-container">
-      <Navbar.Brand href="#" className="nav-title">
+      <Navbar.Brand href="/timeline" className="nav-title">
         Clean India App
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="my-nav" />
@@ -29,10 +26,10 @@ const NavBar = () => {
         className="justify-content-center"
       >
         <Nav variant="pills">
-          <Nav.Item key="Profile">
-            <Nav.Link
-              href="/profile"
-              active={activePath === '/profile'}
+          <Nav.Item className="nav-item-content">
+            <NavLink
+              to="/profile"
+              activeClass = "active"
               className="nav-link"
             >
               <Image
@@ -43,18 +40,18 @@ const NavBar = () => {
               />
               {' '}
               {userName}
-            </Nav.Link>
+            </NavLink>
           </Nav.Item>
           {
-            givenLinkName.map((linkName, index) => (
-              <Nav.Item key={linkName}>
-                <Nav.Link
-                  href={givenPaths[index]}
-                  active={givenPaths[index] === activePath}
+            givenLinkName.map((linkName) => (
+              <Nav.Item key={linkName}  className="nav-item-content">
+                <NavLink
+                  to={`/${linkName}`}
+                  activeClass = "active"
                   className="nav-link"
                 >
                   {linkName}
-                </Nav.Link>
+                </NavLink>
               </Nav.Item>
             ))
           }
