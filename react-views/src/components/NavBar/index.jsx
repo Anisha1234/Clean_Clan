ecae3cae8fa81f442e6dde691ed5ce1241df3582
Lan Trung Nguyen <lantrungsir28@gmail.com>
2020-04-09 17:27:23 +0700

    fix navbar syntax error

--- react-views/src/components/NavBar/index.jsx
+++ react-views/src/components/NavBar/index.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
           <Nav.Item className="nav-item-content">
             <NavLink
               to="/profile"
-              activeClass = "active"
+              activeClassname = "active"
               className="nav-link"
             >
               <Image
